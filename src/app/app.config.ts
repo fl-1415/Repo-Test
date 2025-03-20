c60e9d8068f17dbb20ebe2d6790e504d1e4a3378
Florian Speckmaier <florian.speckmaier@lhind.dlh.de>
2025-03-20 11:54:01 +0100

    init commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,28 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import * as fromProducts from './+state/products.reducer';
+import { ProductsEffects } from './+state/products.effects';
+import * as fromUsers from './+state/users.reducer';
+import { UsersEffects } from './+state/users.effects';
+import { UsersFacade } from './+state/users.facade';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideEffects(UsersEffects),
+    provideState(fromUsers.USERS_FEATURE_KEY, fromUsers.usersReducer),
+    UsersFacade,
+    provideStore(),
+    provideEffects(),
+    provideEffects(ProductsEffects),
+    provideState(
+      fromProducts.PRODUCTS_FEATURE_KEY,
+      fromProducts.productsReducer
+    ),
+    provideStore(),
+    provideEffects(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
   ],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+
+  {
+    path: 'orders',
+    loadComponent: () =>
+      import('@test-angular-nx/orders').then((m) => m.OrdersComponent),
+  },
+
+];
